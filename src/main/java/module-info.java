e9d948c29db06abdcff34e1c3fd7d2a79e64cd45
eladkhil <eladkhil119@gmail.com>
2024-04-18 17:07:44 +0100

    interface backoffice

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.tourjoy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.tourjoy to javafx.fxml;
-    exports com.example.tourjoy;
-}
