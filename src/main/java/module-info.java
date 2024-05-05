d12d44ef7e3f34235ae8ff21629a51a8a82101dd
iheb09 <iheb.souissi@esprit.tn>
2024-05-05 17:54:58 +0200

    CRUD + APIs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,18 @@
-module com.example.tourjoy {
-    requires javafx.controls;
+module com.tourjoy.ihebtransportt {
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.sql;
+    requires com.gluonhq.charm.glisten;
+    requires com.gluonhq.attach.util;
+    requires org.apache.pdfbox;
+    requires java.desktop;
+    requires okhttp3;
+    requires com.google.zxing;
+    requires com.google.zxing.javase;
 
-    requires com.dlsc.formsfx;
-
-    opens com.example.tourjoy to javafx.fxml;
-    exports com.example.tourjoy;
+    requires freetts;
+    opens Controller to javafx.fxml; // This line opens the Controller package to the javafx.fxml module
+    exports com.tourjoy.ihebtransportt;
+    opens Entities to javafx.base;
 }
